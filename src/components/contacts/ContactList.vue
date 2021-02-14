5e99ebddb825caaa0e0b1a90e5745c51648a94b1
Lars Richter <github@n-develop.com>
2021-02-14 20:55:33 +0100

    Added the contact list that uses the contact-teaser

---
+++ src/components/contacts/ContactList.vue
@@ -0,0 +1,36 @@
+<template>
+  <div
+    id="people-feed"
+    class="column is-4-desktop is-full-mobile feed hero is-fullheight"
+  >
+    <contact-teaser></contact-teaser>
+    <contact-teaser></contact-teaser>
+    <contact-teaser></contact-teaser>
+    <contact-teaser></contact-teaser>
+    <contact-teaser></contact-teaser>
+    <contact-teaser></contact-teaser>
+    <contact-teaser></contact-teaser>
+  </div>
+</template>
+
+<script>
+import ContactTeaser from "./ContactTeaser.vue";
+
+export default {
+  components: {
+    ContactTeaser,
+  },
+};
+</script>
+
+<style scoped>
+.feed {
+  background-color: #2b677e;
+  overflow-y: scroll;
+}
+
+.feed > .card {
+  margin-top: 10px;
+  margin-right: 10px;
+}
+</style>
