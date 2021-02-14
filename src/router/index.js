615b3c909899dbb877fda5f66ba878e9cda86071
Lars Richter <github@n-develop.com>
2021-02-14 20:56:17 +0100

    Make the main-page the root for now. Auth will come a bit later

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,15 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import TheLogin from '../views/TheLogin.vue'
+import MainPage from '../views/MainPage.vue'
 
 const routes = [
   {
     path: '/',
+    name: 'MainPage',
+    component: MainPage
+  },
+  {
+    path: '/login',
     name: 'Login',
     component: TheLogin
   }
