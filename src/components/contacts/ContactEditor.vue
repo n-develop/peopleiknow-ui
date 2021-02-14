267699c58c505790ea76f9d55d13111e81a670bb
Lars Richter <github@n-develop.com>
2021-02-14 20:54:50 +0100

    Added a contact-editor component that should hold the form later

---
+++ src/components/contacts/ContactEditor.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="people-pane" class="column is-8-desktop is-hidden-mobile pane">
+    <div class="columns is-desktop is-vcentered" style="height: 100%">
+      <div class="column">
+        <h2 class="has-text-centered">Choose a Contact from the list!</h2>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.pane {
+  background-color: #91d2ec;
+  overflow-y: scroll;
+}
+
+.pane > .box {
+  margin: 10px 10px 0 0;
+}
+</style>
