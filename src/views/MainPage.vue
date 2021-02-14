bf077bd8b5e55f1ea5eaae72025be6099a915afb
Lars Richter <github@n-develop.com>
2021-02-14 20:55:55 +0100

    added a main-page view to show the list and the editor side-by-side

---
+++ src/views/MainPage.vue
@@ -0,0 +1,29 @@
+<template>
+  <the-navigation></the-navigation>
+  <div id="pik-app" class="columns">
+    <contact-list></contact-list>
+    <contact-editor></contact-editor>
+  </div>
+</template>
+
+<script>
+import ContactList from "../components/contacts/ContactList.vue";
+import ContactEditor from "../components/contacts/ContactEditor.vue";
+import TheNavigation from "../components/layout/TheNavigation.vue";
+
+export default {
+  components: {
+    ContactList,
+    ContactEditor,
+    TheNavigation,
+  },
+};
+</script>
+
+<style scoped>
+#pik-app {
+  margin-top: 0;
+  margin-left: 0;
+  margin-bottom: 0;
+}
+</style>
