cb909b09dc481c1504f6c803614dd12efdd89113
Lars Richter <github@n-develop.com>
2021-02-14 20:04:21 +0100

    Added a login-page and made it the root url

---
+++ src/views/TheLogin.vue
@@ -0,0 +1,59 @@
+<template>
+  <section id="login-screen" class="hero is-fullheight">
+    <div class="hero-body">
+      <div class="container">
+        <div class="columns is-centered">
+          <div class="column is-5-tablet is-4-desktop is-3-widescreen">
+            <form class="box" id="login" @submit.prevent>
+              <div class="field">
+                <label class="label">Username</label>
+                <div class="control has-icons-left">
+                  <span class="icon is-small is-left">
+                    <i class="fa fa-user-circle"></i>
+                  </span>
+                  <input
+                    class="input"
+                    aria-label="Username"
+                    autofocus
+                    required
+                    placeholder="Username"
+                  />
+                </div>
+              </div>
+              <div class="field">
+                <label class="label">Password</label>
+                <div class="control has-icons-left">
+                  <span class="icon is-small is-left">
+                    <i class="fa fa-lock"></i>
+                  </span>
+                  <input
+                    class="input"
+                    type="password"
+                    aria-label="Password"
+                    required
+                    placeholder="Password"
+                  />
+                </div>
+              </div>
+              <div class="field">
+                <label class="checkbox">
+                  <input type="checkbox" />
+                  Remember me
+                </label>
+              </div>
+              <div class="field">
+                <button class="button is-info">Login</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+#login-screen {
+  background-color: #2b677e;
+}
+</style>
