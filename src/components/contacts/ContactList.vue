40d4ca68958d6626da2f584716eb6144a76a39c5
Lars Richter <github@n-develop.com>
2021-02-14 21:03:03 +0100

    Added dummy data for contact list

--- src/components/contacts/ContactList.vue
+++ src/components/contacts/ContactList.vue
@@ -20,6 +20,41 @@ export default {
   components: {
     ContactTeaser,
   },
+  data(){
+    return {
+      contactTeaser: [
+        {
+          id: 1,
+          firstname: 'Max',
+          lastname: 'Mustermann',
+          email: 'max@mustermann.de',
+          employer: 'Mustermann AG',
+          businessTitle: 'Head of Controlling',
+          imgPath: '../../assets/images/1.jpg'
+        },
+        {
+          id: 2,
+          firstname: 'Marianne',
+          lastname: 'Mustermann',
+          email: 'marianne@mustermann.de',
+          imgPath: '../../assets/images/2.jpg'
+        },
+        {
+          id: 3,
+          firstname: 'John',
+          lastname: 'Smith'
+        },
+        {
+          id: 4,
+          firstname: 'Kurt',
+          lastname: 'Meyers',
+          email: 'kurt@meyers.com',
+          employer: 'self-employed',
+          businessTitle: 'Cool Dude'
+        }
+      ]
+    };
+  }
 };
 </script>
 
