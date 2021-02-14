1a80cb3c47c4a78cb76d85877ae1cc902b4e48b0
Lars Richter <github@n-develop.com>
2021-02-14 20:53:47 +0100

    Basic styling for the App

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,22 @@
 <template>
-  <router-view/>
+  <router-view />
 </template>
 
 <script>
-import 'bulma/css/bulma.css';
-import '@fortawesome/fontawesome-free/css/all.css';
+import "bulma/css/bulma.css";
+import "@fortawesome/fontawesome-free/css/all.css";
 
-export default {
-  
-}
+export default {};
 </script>
 
 <style>
+html,
+body {
+  font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+  height: 100vh;
+}
 
+.highlight-color {
+    color: #0139D3;
+}
 </style>
